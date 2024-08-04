2f47959aeacbaa6e9a8e25ea5e6ff631e01da507
shehan1994 <anushka.shehan94@gmail.com>
2024-08-04 23:12:54 +0530

    Backend controller change

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,31 +13,11 @@ class DashboardController extends Controller
     {
         $user = $request->user();
 
-        // Total Number of Surveys
-        /*$total = Survey::query()->where('user_id', $user->id)->count();*/
-
         // Total Number of Customer
         $customer_total = Customer::query()->count();
 
         // Total Number of Apartment
         $apartment_total = Apartment::query()->count();
-
-        // Latest Survey
-        /*$latest = Survey::query()->where('user_id', $user->id)->latest('created_at')->first();*/
-
-        // Total Number of answers
-        /*$totalAnswers = SurveyAnswer::query()
-            ->join('surveys', 'survey_answers.survey_id', '=', 'surveys.id')
-            ->where('surveys.user_id', $user->id)
-            ->count();*/
-
-        // Latest 5 answer
-        /*$latestAnswers = SurveyAnswer::query()
-            ->join('surveys', 'survey_answers.survey_id', '=', 'surveys.id')
-            ->where('surveys.user_id', $user->id)
-            ->orderBy('end_date', 'DESC')
-            ->limit(5)
-            ->getModels('survey_answers.*');*/
 
         return [
             'totalCustomer' => $customer_total,
