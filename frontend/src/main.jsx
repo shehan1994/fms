4bd2bf0da90c8658cf5169cb3725998e6612394c
shehan1994 <anushka.shehan94@gmail.com>
2024-10-10 00:16:32 +0530

    Font awsome and chart import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import { ContextProvider } from './contexts/ContextProvider';
 import store from './redux/store';
 import './index.css';
 import router from './router.jsx';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
