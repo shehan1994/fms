8fe27ff04205e78d24a6a8f95d1276b1904f15ad
shehan1994 <anushka.shehan94@gmail.com>
2024-10-21 00:49:02 +0530

    job posission change

--- frontend/src/views/JobCard/JobCardView.jsx
+++ frontend/src/views/JobCard/JobCardView.jsx
@@ -236,23 +236,6 @@ const aprtmentId = event.target.value
 
                 <div className="col-span-6 sm:col-span-3">
                   <label
-                    htmlFor="employee"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    Assign To
-                  </label>
-                  <Select
-                    id="employee"
-                    value={selectedEmployee}
-                    onChange={handleEmployeeChange}
-                    options={employeeOptions}
-                    placeholder="Select a employee"
-                    onInputChange={fetchEmployees}
-                  />
-                </div>
-
-                <div className="col-span-6 sm:col-span-3">
-                  <label
                     htmlFor="job_task"
                     className="block text-sm font-medium text-gray-700"
                   >
@@ -270,6 +253,25 @@ const aprtmentId = event.target.value
                     className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                   />
                 </div>
+
+                <div className="col-span-6 sm:col-span-3">
+                  <label
+                    htmlFor="employee"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Assign To
+                  </label>
+                  <Select
+                    id="employee"
+                    value={selectedEmployee}
+                    onChange={handleEmployeeChange}
+                    options={employeeOptions}
+                    placeholder="Select a employee"
+                    onInputChange={fetchEmployees}
+                  />
+                </div>
+
+                
 
                 <div className="col-span-6 sm:col-span-3">
                   <label
